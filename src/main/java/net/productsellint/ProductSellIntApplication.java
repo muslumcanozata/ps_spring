d4d9930745a1aaf5baf8e427aa1764bb9a86b507
Muslum Can OZATA <mozata@netas.com.tr>
2021-10-20 11:22:04 +0300

    first commit

---
+++ src/main/java/net/productsellint/ProductSellIntApplication.java
@@ -0,0 +1,13 @@
+package net.productsellint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSellIntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSellIntApplication.class, args);
+    }
+
+}
