4d5c4f6b676730a66088c194bca598b79d5687cf
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-10-20 20:56:46 +0300

    productDao interface is created

---
+++ src/main/java/net/productsellint/dataAccess/abstracts/ProductDao.java
@@ -0,0 +1,7 @@
+package net.productsellint.dataAccess.abstracts;
+
+import net.productsellint.entities.concretes.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductDao extends JpaRepository {
+}
