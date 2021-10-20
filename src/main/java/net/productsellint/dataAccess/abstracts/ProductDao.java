315645feacc00b9dd88ac0a082b747dfb88ffc38
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-10-20 22:06:14 +0300

    jparepository is created for product table

--- src/main/java/net/productsellint/dataAccess/abstracts/ProductDao.java
+++ src/main/java/net/productsellint/dataAccess/abstracts/ProductDao.java
@@ -3,5 +3,6 @@ package net.productsellint.dataAccess.abstracts;
 import net.productsellint.entities.concretes.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductDao extends JpaRepository {
+public interface ProductDao extends JpaRepository<Product, Integer> {
+    Product getByProductName(String productName);
 }
