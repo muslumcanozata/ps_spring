a5abea6e6dd90d1146f63c582ab7016662d83e60
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-10-21 20:56:31 +0300

    productentity is renamed

--- src/main/java/net/productsellint/dataAccess/abstracts/ProductDao.java
+++ src/main/java/net/productsellint/dataAccess/abstracts/ProductDao.java
@@ -1,8 +1,8 @@
 package net.productsellint.dataAccess.abstracts;
 
-import net.productsellint.entities.concretes.Product;
+import net.productsellint.entities.concretes.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductDao extends JpaRepository<Product, Integer> {
-    Product getByProductName(String productName);
+public interface ProductDao extends JpaRepository<ProductEntity, Integer> {
+    ProductEntity getByProductName(String productName);
 }
