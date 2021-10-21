56ade077b42fa870ca4fcd3da9b54a6cc411df7c
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-10-21 20:48:58 +0300

    a dto is created for amount type entity

---
+++ src/main/java/net/productsellint/dataTransferObjects/concretes/AmountTypeDto.java
@@ -0,0 +1,13 @@
+package net.productsellint.dataTransferObjects.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AmountTypeDto {
+    private Integer id;
+    private String type;
+}
