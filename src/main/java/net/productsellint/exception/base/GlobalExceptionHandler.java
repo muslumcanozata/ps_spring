68d34b78ab229e96f03830e1b1999f2b57cbec17
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-10-24 15:19:39 +0300

    global exception handler is implemented

---
+++ src/main/java/net/productsellint/exception/base/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package net.productsellint.exception.base;
+
+
+import lombok.extern.slf4j.Slf4j;
+import net.productsellint.core.utilities.results.ErrorResult;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.bind.ValidationException;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorResult> handleRuntimeException(Exception ex) {
+        log.error("[RuntimeException].", ex);
+        return ResponseEntity.status(500).body(new ErrorResult(ex.getMessage()));
+    }
+
+    @ExceptionHandler(ApplicationException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorResult> handleApplicationException(Exception ex) {
+        ApplicationException exception = (ApplicationException) ex;
+        log.error("[ApplicationException]. Exception: {0}", exception);
+        return ResponseEntity.status(500).body(new ErrorResult(exception.getMessage()));
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorResult> handleValidationException(Exception ex) {
+        ValidationException exception = (ValidationException) ex;
+        log.error("[ValidationException]. Exception: {0}", exception);
+        return ResponseEntity.status(400).body(new ErrorResult(exception.getMessage()));
+    }
+}
