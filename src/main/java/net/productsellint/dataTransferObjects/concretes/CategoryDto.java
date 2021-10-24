682a14726545c54f2793fbdb48f453d0df09fde3
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-10-24 15:17:08 +0300

    notnull annotation is used and status variable is created

--- src/main/java/net/productsellint/dataTransferObjects/concretes/CategoryDto.java
+++ src/main/java/net/productsellint/dataTransferObjects/concretes/CategoryDto.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryDto {
+    @NotNull(message = "id boş bırakılamaz")
     private Integer id;
+    @NotNull(message = "isim boş bırakılamaz")
     private String categoryName;
+    @NotNull(message = "status boş bırakılamaz")
+    private Integer status;
 }
--- src/main/java/net/productsellint/dataTransferObjects/concretes/ProductDto.java
+++ src/main/java/net/productsellint/dataTransferObjects/concretes/ProductDto.java
@@ -4,16 +4,27 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.productsellint.entities.concretes.ProductEntity;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 public class ProductDto {
+    @NotNull(message = "id boş bırakılamaz")
     private Integer id;
+    @NotNull(message = "isim boş bırakılamaz")
     private String productName;
+    @NotNull(message = "ücret boş bırakılamaz")
     private Double unitPrice;
+    @NotNull(message = "kategori id boş bırakılamaz")
     private Integer categoryId;
+    @NotNull(message = "kategori ismi boş bırakılamaz")
     private String categoryName;
+    @NotNull(message = "type id boş bırakılamaz")
     private Integer amountTypeId;
+    @NotNull(message = "type ismi boş bırakılamaz")
     private String amountTypeName;
+    @NotNull(message = "status boş bırakılamaz")
+    private Integer status;
 
     public ProductDto(ProductEntity productEntity) {
         this.id = productEntity.getId();
@@ -23,5 +34,6 @@ public class ProductDto {
         this.categoryName = productEntity.getCategoryEntity().getCategoryName();
         this.amountTypeId = productEntity.getAmountTypeEntity().getId();
         this.amountTypeName = productEntity.getAmountTypeEntity().getType();
+        this.status = productEntity.getEntityStatus().getValue();
     }
 }
