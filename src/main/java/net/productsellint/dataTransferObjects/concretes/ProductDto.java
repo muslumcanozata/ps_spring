97362f2001e28e09e68757320dd9c96abaf9b6c9
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-10-27 12:23:04 +0300

    modelmapper is used instead of a constructor

--- src/main/java/net/productsellint/dataTransferObjects/concretes/ProductDto.java
+++ src/main/java/net/productsellint/dataTransferObjects/concretes/ProductDto.java
@@ -24,16 +24,5 @@ public class ProductDto {
     @NotNull(message = "type ismi boş bırakılamaz")
     private String amountTypeName;
     @NotNull(message = "status boş bırakılamaz")
-    private Integer status;
-
-    public ProductDto(ProductEntity productEntity) {
-        this.id = productEntity.getId();
-        this.productName = productEntity.getProductName();
-        this.unitPrice = productEntity.getUnitPrice();
-        this.categoryId = productEntity.getCategoryEntity().getId();
-        this.categoryName = productEntity.getCategoryEntity().getCategoryName();
-        this.amountTypeId = productEntity.getAmountTypeEntity().getId();
-        this.amountTypeName = productEntity.getAmountTypeEntity().getType();
-        this.status = productEntity.getEntityStatus().getValue();
-    }
+    private Enum status;
 }
