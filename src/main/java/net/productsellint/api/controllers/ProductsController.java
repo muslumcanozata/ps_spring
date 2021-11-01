254941277f70302169dfc57ba0385d9dafb6e9cb
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-11-01 22:27:51 +0300

    crossorigin is added

--- src/main/java/net/productsellint/api/controllers/ProductsController.java
+++ src/main/java/net/productsellint/api/controllers/ProductsController.java
@@ -1,21 +1,18 @@
 package net.productsellint.api.controllers;
 
 import net.productsellint.business.concretes.ProductServiceImpl;
-import net.productsellint.core.utilities.results.Result;
 import net.productsellint.core.utilities.results.SuccessResult;
 import net.productsellint.dataTransferObjects.concretes.ProductDto;
 import net.productsellint.entities.concretes.EntityStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/products")
 public class ProductsController {
