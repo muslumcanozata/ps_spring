30ff381618bbf7a251b9b177409803b8c0b96c99
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-11-02 23:35:50 +0300

    some field changes

--- src/main/java/net/productsellint/dataTransferObjects/concretes/CategoryDto.java
+++ src/main/java/net/productsellint/dataTransferObjects/concretes/CategoryDto.java
@@ -3,19 +3,12 @@ package net.productsellint.dataTransferObjects.concretes;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.productsellint.entities.concretes.CategoryEntity;
-import net.productsellint.entities.concretes.EntityStatus;
-
-import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryDto {
-    @NotNull(message = "id boş bırakılamaz")
     private Integer id;
-    @NotNull(message = "isim boş bırakılamaz")
     private String categoryName;
-    @NotNull(message = "status boş bırakılamaz")
-    private Enum status;
+    private Integer status;
 }
