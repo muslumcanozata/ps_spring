665bb70d46cb5a56b3e54137f9bf34822294b809
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-11-02 23:36:06 +0300

    some field changes

--- src/main/java/net/productsellint/dataTransferObjects/concretes/OrderDto.java
+++ src/main/java/net/productsellint/dataTransferObjects/concretes/OrderDto.java
@@ -4,41 +4,32 @@ import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.productsellint.entities.concretes.AmountTypeEntity;
-import net.productsellint.entities.concretes.CategoryEntity;
-import net.productsellint.entities.concretes.EntityStatus;
-import net.productsellint.entities.concretes.ProductEntity;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDto {
-    @NotNull(message = "id boş bırakılamaz")
     private Integer id;
-    @NotNull(message = "uuid boş bırakılamaz")
     private UUID uuid;
-    @NotNull(message = "ürün id boş bırakılamaz")
-    private Integer productId;
-    @NotNull(message = "ürün ismi boş bırakılamaz")
-    private String productName;
-    @NotNull(message = "kategori id boş bırakılamaz")
-    private Integer categoryId;
-    @NotNull(message = "kategori ismi boş bırakılamaz")
-    private String categoryName;
-    @NotNull(message = "type id boş bırakılamaz")
-    private Integer amountTypeId;
-    @NotNull(message = "type ismi boş bırakılamaz")
-    private String amountTypeName;
-    @NotNull(message = "miktar boş bırakılamaz")
-    private Float amount;
-    @NotNull(message = "tarih boş bırakılamaz")
+    @NotNull(message = "ürün listesi boş bırakılamaz")
+    private List<OrderSingleRequest> orderList;
+    @NotNull(message = "Varış zamanı boş bırakılamaz")
+    @Temporal(TemporalType.DATE)
+    private Date deliveryForecast;
+    @NotNull(message = "user id boş bırakılamaz")
+    private Integer userId;
     @DateTimeFormat(pattern = "dd/MM/yyyy hh:mm:ss")
-    private Date date;
+    private Date createdDate;
+    @DateTimeFormat(pattern = "dd/MM/yyyy hh:mm:ss")
+    private Date updatedDate;
+    @NotNull(message = "kargo durumu boş bırakılamaz")
+    private Integer deliveryStatus;
     @NotNull(message = "status boş bırakılamaz")
-    private Enum status;
+    private Integer status;
 }
--- src/main/java/net/productsellint/entities/concretes/OrderEntity.java
+++ src/main/java/net/productsellint/entities/concretes/OrderEntity.java
@@ -1,17 +1,21 @@
 package net.productsellint.entities.concretes;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.hibernate.annotations.*;
 
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.Date;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 @Entity
 @Table(name = "orders")
 public class OrderEntity {
@@ -20,29 +24,44 @@ public class OrderEntity {
     @Column(name = "id", insertable = false, updatable = false)
     private Integer id;
 
-    @Column(name = "uuid", nullable = false)
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(name = "uuid", updatable = false, nullable = false)
     private UUID uuid;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", referencedColumnName = "id")
-    private ProductEntity productEntity;
+    @Type(type = "jsonb")
+    @Column(name = "order_list")
+    private String orderList;
+
+    @Column(name = "delivery_forecast", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date deliveryForecast;
+
+    @Column(name = "created_date", nullable = false)
+    @CreationTimestamp
+    private Date createdDate;
+
+    @Column(name = "updated_date", nullable = false)
+    @UpdateTimestamp
+    private Date updatedDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id", referencedColumnName = "id")
-    private CategoryEntity categoryEntity;
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity userEntity;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "amounttype_id", referencedColumnName = "id")
-    private AmountTypeEntity amountTypeEntity;
-
-    @Column(name = "amount", nullable = false)
-    private Float amount;
-
-    @Column(name = "date", nullable = false)
-    @DateTimeFormat(pattern = "dd/MM/yyyy hh:mm:ss")
-    private Date date;
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "delivery_status")
+    private DeliveryStatus deliveryStatus;
 
     @Enumerated(EnumType.ORDINAL)
     @Column(name = "status")
     private EntityStatus entityStatus;
+
+    public OrderEntity(String orderList, Date deliveryForecast, Integer userId, DeliveryStatus deliveryStatus, EntityStatus entityStatus) {
+        this.orderList = orderList;
+        this.deliveryForecast = deliveryForecast;
+        this.userEntity.setId(userId);
+        this.deliveryStatus = deliveryStatus;
+        this.entityStatus = entityStatus;
+    }
 }
