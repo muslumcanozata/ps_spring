c73ce1115bab0880d8d7c99f754e3e9bc8262649
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-11-05 14:30:55 +0300

    getStatus method is created

--- src/main/java/net/productsellint/entities/concretes/EntityStatus.java
+++ src/main/java/net/productsellint/entities/concretes/EntityStatus.java
@@ -1,5 +1,9 @@
 package net.productsellint.entities.concretes;
 
+import net.productsellint.exception.custom.EntityStatusNotFoundException;
+
+import java.util.Optional;
+
 public enum EntityStatus {
     ACTIVE(0), DELETED(1), DISABLED(2);
     private final Integer value;
@@ -12,4 +16,18 @@ public enum EntityStatus {
         return this.value;
     }
 
+    public static EntityStatus getStatus(Integer value) {
+        if (value == 0) {
+            return EntityStatus.ACTIVE;
+        }
+        else if(value == 1) {
+            return EntityStatus.DELETED;
+        }
+        else if(value == 2) {
+            return EntityStatus.DISABLED;
+        }
+        else {
+             throw new EntityStatusNotFoundException(value);
+        }
+    }
 }
